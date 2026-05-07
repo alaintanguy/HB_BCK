99f31ecabe46497a8b55d745b956c9c8caef2830
alain M samoun <samoun.alain@gmail.com>
2026-05-06 17:50:00 -0700

    Spinner UI working

--- app/src/main/java/com/healthbridge/MainActivity.kt
+++ app/src/main/java/com/healthbridge/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.healthbridge
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.deviceSpinner)
+
+        val devices = listOf(
+            "Polar H10",
+            "Garmin HRM",
+            "Wahoo Tickr"
+        )
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            devices
+        )
+
+        spinner.adapter = adapter
     }
 }
